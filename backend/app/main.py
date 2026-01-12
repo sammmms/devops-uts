fb5bb5f96f7df30bf827e5e41fafe55f27c8adb8
sam <samuelonasis23@gmail.com>
2026-01-12 14:18:31 +0700

    chore: some changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,15 +7,6 @@ from app.api.routes_category import router as category_router
 from app.api.routes_category_todo import router as category_todo_router
 from app.utils.response_util import create_json_response
 
-routes: list[dict[str:APIRouter]] = [
-    {
-        "todos": todo_router,
-        "categories": category_router,
-        "category_todos": category_todo_router,
-    }
-]
-
-
 async def http_exception_handler(_, exc: HTTPException):
     return JSONResponse(
         status_code=exc.status_code,
@@ -32,6 +23,21 @@ def add_cors_middleware(app: FastAPI):
         allow_headers=["*"],
     )
 
+router = APIRouter()
+
+@router.get("/health", tags=["health"])
+def health():
+    return create_json_response(message="API is running")
+
+routes: list[dict[str:APIRouter]] = [
+    {
+        "health": router,
+        "todos": todo_router,
+        "categories": category_router,
+        "category_todos": category_todo_router,
+    }
+]
+
 
 def create_app() -> FastAPI:
     app = FastAPI(
@@ -44,10 +50,6 @@ def create_app() -> FastAPI:
     app.add_exception_handler(HTTPException, http_exception_handler)
 
     add_cors_middleware(app)
-
-    app.get("/", tags=["health"])(
-        lambda: create_json_response(message="API is running")
-    )
 
     for route in routes:
         for prefix, router in route.items():
--- frontend/src/utils/axios_instance.tsx
+++ frontend/src/utils/axios_instance.tsx
@@ -1,7 +1,8 @@
+import { API_BASE_URL } from "@/config";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
